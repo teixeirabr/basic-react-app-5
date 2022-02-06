a59c9e29b2f81e4f86348f2d7a22756331517d5e
Andre <andre@A-MacBook-Air.local>
2022-02-06 18:29:48 -0500

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Image from "./components/Image";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Image url={"http://earthporm.com/wp-content/uploads/2014/07/animal-children-photography-elena-shumilova-2.jpg"} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/Image.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Image = () => {
+  return <div>
+       <img src={this.props.url}/>
+  </div>;
+};
+
+export default Image;
